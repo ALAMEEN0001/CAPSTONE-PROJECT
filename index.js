32ba297b55239a8d359ddb569f7534aa84a7f728
alameenayodele2008@gmail.com <alameenayodele2008@gmail.com>
2023-12-14 21:44:00 +0100

    update

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const navtoggle = document.querySelector('.navtoggler');
-const nav = document.querySelector('.navbar');
+
 const btnclose = document.querySelector('.button');
 document.getElementsByClassName('.navbar')
 const slider = document.getElementsByClassName('.slider')
-
+var nav = document.getElementById('site-menu');
+var header = document.getElementById('top');
+var btn = document.getElementById('menuBtn');
+var nav = document.getElementById('menu');
 
 navtoggle.addEventListener('click', () => {
   
@@ -18,6 +21,25 @@ btnclose.addEventListener('click', () => {
     navtoggle.classList.toggle('hidden');
     btnclose.classList.toggle('hidden');
 });
+
+
+window.addEventListener('scroll', function() {
+  if (window.scrollY >=400) { // adjust this value based on site structure and header image height
+    nav.classList.add('nav-sticky');
+    header.classList.add('pt-scroll');
+  } else {
+    nav.classList.remove('nav-sticky');
+    header.classList.remove('pt-scroll');
+  }
+});
+
+function navToggle() {
+   
+
+        btn.classList.toggle('open');
+        nav.classList.toggle('flex');
+        nav.classList.toggle('hidden');
+    }
 
 function Next1(){
     next();
